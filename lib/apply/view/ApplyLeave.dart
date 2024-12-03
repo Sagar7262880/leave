0974c4d1dc3942270e319bb1c07bf08077014b14
Sagar-at-079925877420 <sagar.s@girdmail.com>
2024-12-03 11:54:54 +0530

    .

--- lib/apply/view/ApplyLeave.dart
+++ lib/apply/view/ApplyLeave.dart
@@ -38,87 +38,93 @@ class _ApplyleaveState extends State<Applyleave> {
         iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.only(top: 10, left: 8, right: 8),
-        decoration: BoxDecoration(
-          // color: white80,
-          color: Colors.white.withOpacity(.9),
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  // mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "APPLY LEAVE",
-                      style: TextStyle(
-                          color: Colors.grey, fontWeight: FontWeight.w800),
-                    ),
-                  ],
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.only(top: 10, left: 8, right: 8),
+              decoration: BoxDecoration(
+                // color: white80,
+                color: Colors.white.withOpacity(.9),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
-                const SizedBox(
-                  height: 24,
-                ),
-                SimpleDropdown(
-                    labelText: "Leave Type",
-                    items: const [
-                      "Privilege",
-                      "Sick",
-                      "Unpaid",
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        // mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "APPLY LEAVE",
+                            style: TextStyle(
+                                color: Colors.grey, fontWeight: FontWeight.w800),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      SimpleDropdown(
+                          labelText: "Leave Type",
+                          items: const [
+                            "Privilege",
+                            "Sick",
+                            "Unpaid",
+                          ],
+                          onChanged: (value) {}),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      DatePickerField(label: "From Date", controller: fromDateCtr),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      DatePickerField(label: "To Date", controller: toDateCtr),
+                      const SizedBox(
+                        height: 12,
+                      ),
+            
+                      SimpleDropdown(
+                          labelText: "Select CC",
+                          items: const [
+                            "Sagar salunke",
+                            "Ashitosh Deshmukh",
+                            "Krishna tarke",
+                            "Mahesh Nalwade"
+                          ],
+                          onChanged: (value) {}),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      SimpleDropdown(
+                          labelText: "Reason",
+                          items: const [
+                            "Personal",
+                            "Official",
+                            "Emergency",
+                          ],
+                          onChanged: (value) {}),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("Apply Leave"),
+                      )
                     ],
-                    onChanged: (value) {}),
-                const SizedBox(
-                  height: 12,
+                  ),
                 ),
-                DatePickerField(label: "From Date", controller: fromDateCtr),
-                const SizedBox(
-                  height: 12,
-                ),
-                DatePickerField(label: "To Date", controller: toDateCtr),
-                const SizedBox(
-                  height: 12,
-                ),
-
-                SimpleDropdown(
-                    labelText: "Select CC",
-                    items: const [
-                      "Sagar salunke",
-                      "Ashitosh Deshmukh",
-                      "Krishna tarke",
-                      "Mahesh Nalwade"
-                    ],
-                    onChanged: (value) {}),
-                const SizedBox(
-                  height: 12,
-                ),
-                SimpleDropdown(
-                    labelText: "Reason",
-                    items: const [
-                      "Personal",
-                      "Official",
-                      "Emergency",
-                    ],
-                    onChanged: (value) {}),
-                const SizedBox(
-                  height: 12,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Apply Leave"),
-                )
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
